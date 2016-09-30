ba9349b65cfdef01564705ceaa43f4815f53cfe9
Nikita <nks.off@gmail.com>
2016-09-30 19:04:00 +0300

    add matplotlib

--- vk.py
+++ vk.py
@@ -1,7 +1,9 @@
 from id_from_username import IdFromUsername
 from friends import Friends
+import matplotlib.pyplot as plot
 
-uclient = IdFromUsername('reigning')
+uname = input('Имя пользователя: ')
+uclient = IdFromUsername(uname)
 uid = uclient.execute()
 
 friends_client = Friends(uid)
@@ -9,3 +11,12 @@ friends = friends_client.execute()
 
 for (age, count) in friends:
     print('{} {}'.format(int(age), '#' * count))
+
+fig, ax = plot.subplots()
+rects = ax.bar(list(map(lambda x: x[0], friends)), list(map(lambda x: x[1], friends)), 1)
+
+plot.xlabel('Возраст')
+plot.ylabel('Кол-во')
+plot.title('Распределение друзей по возрастам')
+plot.subplots_adjust()
+plot.show()
